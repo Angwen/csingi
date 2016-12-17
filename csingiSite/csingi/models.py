b0ceb1ab458ec5b6048f2489afd07e7e2447adfc
Zsuzsanna Urmossy <zsuzsanna.urmossy@gravityrd.com>
2016-12-17 16:11:15 +0100

    Database models refined.

--- csingiSite/csingi/models.py
+++ csingiSite/csingi/models.py
@@ -9,7 +9,7 @@ class Players(models.Model):
     e_mail = models.EmailField()
 
 class CharacterStats(models.Model):
-    name = models.CharField(max_length=25)
+    character_name = models.ForeignKey(CharacterStory, on_delete=models.CASCADE)
     test = models.IntegerField()
     lelek = models.IntegerField()
 
@@ -23,20 +23,56 @@ class CharacterStats(models.Model):
 
 
 class CharacterStory(models.Model):
+    character_name = models.CharField(max_length=25)
     lost = models.DateField()
     found = models.DateField()
-    seeming_kith = models.CharField()
-    elrablo = models.CharField()
-    
+    seeming_kith = models.CharField(max_length=25)
+    elrablo = models.CharField(max_length=25)
+    udvar =models.ForeignKey(Udvarok, on_delete=models.CASCADE)
+    uralkodo = models.BooleanField()
+
+
+class CharacterMagic(models.Model):
+    character_name = models.ForeignKey(CharacterStats, on_delete=models.CASCADE)
+    magic_name = models.ForeignKey(Contracts, on_delete=models.CASCADE)
+
 
 class CharacterSkillsBackgrounds(models.Model):
-    name = models.ForeignKey()
+    character_name = models.ForeignKey(CharacterStory, on_delete=models.CASCADE)
     skill_background_name = models.ForeignKey
 
 
 class SkillsBackgrounds(models.Model):
     name = models.CharField(max_length=25)
-    type = models.Field.choices(('skill'), ('background'))
+    tipus = models.Field.choices(('skill'), ('background'))
+    magical = models.BooleanField()
+    clarity_related = models.BooleanField()
     description = models.TextField()
 
 
+class Contracts(models.Model):
+    name = models.CharField(max_length=25)
+    tipus = models.Field.choices(('court'), ('general'), ('egyedi'))
+    description = models.TextField()
+
+
+class Pledges(models.Model):
+    character_name = models.ForeignKey(CharacterStory, on_delete=models.CASCADE)
+    pledge_name = models.CharField(max_length=25)
+    tipus = models.CharField(max_length=25) # eg. city, click, etc.
+    start = models.TimeField()
+    end = models.TimeField()
+    duration = models.Field.choices(('napszak'), ('haromnap'), ('honap'), ('evszak'), ('ev'), ('halalig'), ('orokke')) #kellenek majd a szamok melle
+    duration_metrics = models.IntegerField()
+    description = models.TextField()
+    boon = models.CharField(max_length=25)
+    boon_metrics = models.IntegerField()
+    curse = models.CharField(max_length=25)
+    curse_metrics = models.IntegerField()
+
+
+class Udvarok(models.Model):
+    court_name = models.CharField(max_length=10)
+    court_contracts = models.ForeignKey(Contracts, on_delete=models.CASCADE)
+    uralkodo = models.ForeignKey(CharacterStory, on_delete=models.CASCADE))
+    description = models.TextField()
