e07a479ed4a7a1b91a3a86a88051a8ead2d01046
Zsuzsanna Urmossy <zsuzsanna.urmossy@gravityrd.com>
2016-12-17 16:58:01 +0100

    started creating basic views; debugging on db models.

--- csingiSite/csingi/models.py
+++ csingiSite/csingi/models.py
@@ -2,24 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Players(models.Model):
-    player_name = models.CharField(max_length=25)
-    character_name = models.ForeignKey(CharacterStats, on_delete=models.CASCADE)
-    status = models.BooleanField()
-    e_mail = models.EmailField()
+class Contracts(models.Model):
+    name = models.CharField(max_length=25)
+    tipus = models.CharField(max_length=25) # (('court'), ('general'), ('egyedi'))
+    description = models.TextField()
 
-class CharacterStats(models.Model):
-    character_name = models.ForeignKey(CharacterStory, on_delete=models.CASCADE)
-    test = models.IntegerField()
-    lelek = models.IntegerField()
 
-    elme = models.IntegerField()
-    elet = test * 2
-    akarat = lelek + elme
-
-    vegzet = models.IntegerField()
-    varazs = vegzet + 9
-    tisztanlatas = models.IntegerField()
+class Udvarok(models.Model):
+    court_name = models.CharField(max_length=10)
+    court_contracts = models.ForeignKey(Contracts, on_delete=models.CASCADE)
+#    uralkodo = models.ForeignKey(CharacterStory, on_delete=models.CASCADE)
+    description = models.TextField()
 
 
 class CharacterStory(models.Model):
@@ -30,6 +23,38 @@ class CharacterStory(models.Model):
     elrablo = models.CharField(max_length=25)
     udvar =models.ForeignKey(Udvarok, on_delete=models.CASCADE)
     uralkodo = models.BooleanField()
+    status = models.BooleanField()
+
+
+class Players(models.Model):
+    player_name = models.CharField(max_length=25)
+    character_name = models.ForeignKey(CharacterStory, on_delete=models.CASCADE)
+    e_mail = models.EmailField()
+
+
+class CharacterStats(models.Model):
+    def elet(self):
+        return self.test * 2
+
+    def akarat(self):
+        return self.lelek + self.elme
+
+    def varazs(self):
+        return self.vegzet + 9
+
+    character_name = models.ForeignKey(CharacterStory, on_delete=models.CASCADE)
+    test = models.IntegerField()
+    lelek = models.IntegerField()
+
+    elme = models.IntegerField()
+    elet = property(elet)
+    akarat = property(akarat)
+
+    vegzet = models.IntegerField()
+    varazs = property(vegzet)
+    tisztanlatas = models.IntegerField()
+
+
 
 
 class CharacterMagic(models.Model):
@@ -44,15 +69,9 @@ class CharacterSkillsBackgrounds(models.Model):
 
 class SkillsBackgrounds(models.Model):
     name = models.CharField(max_length=25)
-    tipus = models.Field.choices(('skill'), ('background'))
+    tipus = models.CharField(max_length=25) #(('skill'), ('background'))
     magical = models.BooleanField()
     clarity_related = models.BooleanField()
-    description = models.TextField()
-
-
-class Contracts(models.Model):
-    name = models.CharField(max_length=25)
-    tipus = models.Field.choices(('court'), ('general'), ('egyedi'))
     description = models.TextField()
 
 
@@ -62,7 +81,7 @@ class Pledges(models.Model):
     tipus = models.CharField(max_length=25) # eg. city, click, etc.
     start = models.TimeField()
     end = models.TimeField()
-    duration = models.Field.choices(('napszak'), ('haromnap'), ('honap'), ('evszak'), ('ev'), ('halalig'), ('orokke')) #kellenek majd a szamok melle
+    duration = models.CharField(max_length=25) #(('napszak'), ('haromnap'), ('honap'), ('evszak'), ('ev'), ('halalig'), ('orokke')) #kellenek majd a szamok melle
     duration_metrics = models.IntegerField()
     description = models.TextField()
     boon = models.CharField(max_length=25)
@@ -71,8 +90,3 @@ class Pledges(models.Model):
     curse_metrics = models.IntegerField()
 
 
-class Udvarok(models.Model):
-    court_name = models.CharField(max_length=10)
-    court_contracts = models.ForeignKey(Contracts, on_delete=models.CASCADE)
-    uralkodo = models.ForeignKey(CharacterStory, on_delete=models.CASCADE))
-    description = models.TextField()
--- csingiSite/csingi/views.py
+++ csingiSite/csingi/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import *
 
 # Create your views here.
+
+
+def index():
+    pass
+
+def karakter(request):
+    data = CharacterStory.objects.all()
+    kiskutya = "tiiriitititi"
+    template = loader.get_template("csingi/karakter.html")
+    context = {
+        "data": data,
+        "kiskutya": kiskutya
+    }
+    return HttpResponse(template.render(context, request))
